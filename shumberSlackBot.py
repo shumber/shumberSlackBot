c6c1f0abe68a60c0a498a20686d617e6fba66211
Scott Humber <scott@vitalitics.ca>
2017-10-31 20:20:33 -0230

    added user dicts

--- shumberSlackBot.py
+++ shumberSlackBot.py
@@ -7,22 +7,48 @@ load_dotenv('keys.env')
 
 slack_token = os.environ['SLACK_API_TOKEN']
 sc = SlackClient(slack_token)
-
+'''
 sc.api_call(
   "chat.postMessage",
   channel="#bot_playground",
   text="Hello from scottSlackBot! :tada:"
 )
-
+'''
+userList = {}
 if sc.rtm_connect(): #connect to slack 
+    api_call = sc.api_call("users.list")
+    users = api_call.get('members')
+    for user in users:
+        userList[user['id']] = {}
+        userList[user['id']]['active'] = 0
+        userList[user['id']]['away'] = 0
+        userList[user['id']]['total'] = 0
     while True:
         event = sc.rtm_read()
-        print(event)
-
+        #print(event)
+        if event['type'] == "presence_change":
+            handlePresenceChange(event)
         time.sleep(1)
 else:
     print("Connection Failed")
 
+def handlePresenceChange(event):
+    print("Status change for ", event['user'])
+
+
 #create list of users
+'''
+if sc.rtm_connect():
+    api_call = sc.api_call("users.list")
+    users = api_call.get('members')
+    for user in users:
+        userList[user] = 0
+        
+        #print(user['name'] + ' - ID : ' + user.get('id'))
+'''
+
+
+
+
 #determin when a user joins and leaves a channel
-#create a command to send a user a message
+#create a command to print list of users and time in channel
