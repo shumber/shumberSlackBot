f76ea2df3acc0cf1957922ea67a54c6ee1233f67
Justin Simms <j.simms@mun.ca>
2017-11-02 10:07:24 -0230

    Almost There

--- shumberSlackBot.py
+++ shumberSlackBot.py
@@ -8,68 +8,59 @@ load_dotenv('.env')
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 userList = {}
-'''
-sc.api_call(
-  "chat.postMessage",
-  channel="#bot_playground",
-  text="Hello from scottSlackBot! :tada:"
-)
-'''
 
 def handlePresenceChange(event): #Log the users score as they enter and leave the chat
-    print("Status change for ", event['user'])
     if event['presence'] == 'active':
         userList[event['user']]['active'] = time.time()
+        userList[user['id']]['activeFlag'] = 1
     if event['presence'] == 'away':
         userList[event['user']]['away'] = time.time()
         userList[event['user']]['total'] += (userList[event['user']]['away'] - userList[event['user']]['active'])
+        userList[user['id']]['activeFlag'] = 0
         print(userList[event['user']]['total'])
 
 def handle_message(event):
-    userList[event['user']]['text'] = time.time() ## The time of the message sent
-    chan="#bot_playground"
-    x = userList[event['user']]['text'] - userList[event['user']]['active'] + userList[event['user']]['total'] + 0 #the score at the time of the message 
-    sc.api_call('chat.postMessages', channel=chan, text=x) ##Sumiting the score and message
+    for key, value in userList.items():
+        if value['isBot'] == 1:
+            if value['activeFlag'] == 1:
+                level = (time.time() - userList[event['user']]['active']) + value['total'] #the score at the time of the message 
+            else: 
+                level = value['total']
+            con =( " The Parties score for: " + "\n "+ value['name']+ "is" +str(int(level)))
+    sc.api_call('chat.postMessages', channel="#bot_playground", text=con) ##Sumiting the score and message
 
 
 if sc.rtm_connect(): #connect to slack 
-    api_call = sc.api_call("users.list")
+    api_call = sc.api_call("users.list", presence="true")
     users = api_call.get('members')
     chan="#bot_playground"
-    greeting="Not going to do it pal!" ##Nice to meet you. Type Score to see your RPG total
-    sc.api_call("chat.postMessage", channel=chan, text=greeting)
+    ##greeting="Here we go" ##Nice to meet you. Type Score to see your RPG total
+    ##sc.api_call("chat.postMessage", channel=chan, text=greeting)
     for user in users:
         userList[user['id']] = {}
-        userList[user['id']]['active'] = 0
-        userList[user['id']]['away'] = 0
-        userList[user['id']]['total'] = 0
+        userList[user['id']]['active'] = 0.0
+        userList[user['id']]['away'] = 0.0
+        userList[user['id']]['total'] = 0.0
+        userList[user['id']]['name'] = user['name']
+        userList[user['id']]['activeFlag'] = 0
+        userList[user['id']]['isBot'] = 0
+        if user['id'] != "USLACKBOT":
+            if user['deleted'] == False:
+                if user['is_bot']==False:
+                    userList[user['id']]['isBot']= 1
+                    if user['presence']=="active":
+                        userList[user['id']]['active'] = time.time()
+                        userList[user['id']]['activeFlag'] = 1
+
     while True:
         events = sc.rtm_read()
-        ##print(events)
+        print(events)
         for event in events:
             if event['type'] == "presence_change":
                 handlePresenceChange(event)
-            if event['type'] == "message:":
-                text = event['text']
-                if 'Post' in text:
-                    handleMessage(event)
-            time.sleep(1)
+            elif event['type'] == "message":
+                if event['text'] == "Post":
+                    handle_message(event)
+        time.sleep(1)
 else:
     print("Connection Failed")
-
-#create list of users
-'''
-if sc.rtm_connect():
-    api_call = sc.api_call("users.list")
-    users = api_call.get('members')
-    for user in users:
-        userList[user] = 0
-        
-        #print(user['name'] + ' - ID : ' + user.get('id'))
-'''
-
-
-
-
-#determin when a user joins and leaves a channel
-#create a command to print list of users and time in 
