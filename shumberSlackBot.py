1cf88f86e966aaa3c8a5f7b8c7b7bab8f8c0707b
Justin Simms <j.simms@mun.ca>
2017-11-02 10:48:31 -0230

    Working Log time not working message

--- shumberSlackBot.py
+++ shumberSlackBot.py
@@ -3,60 +3,56 @@ import time
 from slackclient import SlackClient
 from dotenv import load_dotenv
 
-load_dotenv('keys.env')
 
-slack_token = os.environ['SLACK_API_TOKEN']
+load_dotenv('.env')
+slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 userList = {}
 
-def handlePresenceChange(event):
+def handlePresenceChange(event): #Log the users score as they enter and leave the chat
     if event['presence'] == 'active':
         print("Status Active for ", event['user'], " - ", userList[event['user']]['name'])
         userList[event['user']]['active'] = time.time()
-        userList[event['user']]['activeFlag'] = 1 #Flag set when presence changes to active
+        userList[user['id']]['activeFlag'] = 1
     if event['presence'] == 'away':
         print("Status Away for ", event['user'], " - ", userList[event['user']]['name'])
         userList[event['user']]['away'] = time.time()
-        userList[event['user']]['activeFlag'] = 0 #Flag reset when presence changes to away
+        userList[user['id']]['activeFlag'] = 0
         userList[event['user']]['total'] = userList[event['user']]['total'] + (userList[event['user']]['away'] - userList[event['user']]['active'])
+        print(userList[event['user']]['total'])
 
-
-def handleMessage(event):
-    text = "RPG User scores:"
+def handlemessage(event):
+    print("Message from", event['user'], " - ", userList[event['user']]['name'])
     for key, value in userList.items():
-        if value['isBot'] == 0:
-            if value['activeFlag'] == 1: #if a user is active, we want to include their current active time in score without having to wait for a status change to away.
-                totalScore = time.time() - userList[event['user']]['active']
-            else:
-                totalScore = value['total'] #if not active, we just used the stored total score.
-            text += ("\nuser:" + value['name'] + " - Score: " + str(int(totalScore)))            
-    sc.api_call(
-        "chat.postMessage",
-        channel="#bot_playground",
-        text=text
-        )
+        if value['isBot'] == 1:
+            if value['activeFlag'] == 1:
+                level = time.time() - userList[event['user']]['active'] + value['total']  #the score at the time of the message 
+            else: 
+                level = value['total']
+            con =( "The Parties score for: " + "\n "+ value['name']+ "is" +str(int(level)))
+    sc.api_call('chat.postMessages', channel="#bot_playground", text=con) ##Sumiting the score and message
 
 
 if sc.rtm_connect(): #connect to slack 
     api_call = sc.api_call("users.list", presence="true")
     users = api_call.get('members')
+    ##greeting="Here we go" ##Nice to meet you. Type Score to see your RPG total
+    ##sc.api_call("chat.postMessage", channel="#bot_playground", text=greeting)
     for user in users:
         userList[user['id']] = {}
         userList[user['id']]['active'] = 0.0
         userList[user['id']]['away'] = 0.0
         userList[user['id']]['total'] = 0.0
         userList[user['id']]['name'] = user['name']
-        userList[user['id']]['activeFlag'] = 0 #indicates if user is currently active
-        userList[user['id']]['isBot'] = 1 #bot flag
+        userList[user['id']]['activeFlag'] = 0
+        userList[user['id']]['isBot'] = 0
         if user['id'] != "USLACKBOT":
-            if user['deleted'] == False: #Ignore deleted users
-                if user['is_bot'] == False: #Ignore bots
-                    userList[user['id']]['isBot'] = 0 #any users at this point are not bots
-                    if user['presence'] == "active": #if user is active, we want to set their active time without having to wait for a presence change.
+            if user['deleted'] == False:
+                if user['is_bot']==False:
+                    userList[user['id']]['isBot']= 1
+                    if user['presence']=="active":
                         userList[user['id']]['active'] = time.time()
-                        userList[user['id']]['activeFlag'] = 1 
-
-    #print(userList)    
+                        userList[user['id']]['activeFlag'] = 1
     while True:
         events = sc.rtm_read()
         print(events)
@@ -65,7 +61,7 @@ if sc.rtm_connect(): #connect to slack
                 handlePresenceChange(event)
             elif event['type'] == "message":
                 if event['text'] == "Post":
-                    handleMessage(event)
+                    handlemessage(event)
         time.sleep(1)
 else:
     print("Connection Failed")
