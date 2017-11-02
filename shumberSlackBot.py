f5a5c7f0e92ad7bb5658800cdcecf042b2f3eb6f
Justin Simms <j.simms@mun.ca>
2017-11-02 15:19:45 -0230

    Almost There Rev6

--- shumberSlackBot.py
+++ shumberSlackBot.py
@@ -22,6 +22,20 @@ def handlePresenceChange(event): #Log the users score as they enter and leave th
        
 
 def handlemessage(event):
+    #text = "RPG User scores:"
+    for key, value in userList.items():
+        if value['isBot'] == 0:
+            if value['activeFlag'] == 1: #if a user is active, we want to include their current active time in score without having to wait for a status change to away.
+                totalScore = time.time() - userList[event['user']]['active']
+            else:
+                totalScore = value['total'] #if not active, we just used the stored total score.
+            text = ("RPG User scores:"+"\nuser:" + value['name'] + " - Score: " + str(int(totalScore)))            
+    sc.api_call(
+        "chat.postMessage",
+        channel="#bot_playground",
+        text=text
+        )
+    '''
     print("Message from", event['user'], " - ", userList[event['user']]['name'])
     for key, value in userList.items():
         if value['isBot'] == 0:
@@ -31,7 +45,7 @@ def handlemessage(event):
                 level = value['total']
             con =( "The Parties score for: " + "\n "+ value['name']+ "is" +str(int(level)))
     sc.api_call('chat.postMessages', channel="#bot_playground", text=con) ##Sumiting the score and message
-
+'''
 
 if sc.rtm_connect(): #connect to slack 
     api_call = sc.api_call("users.list", presence="true")
