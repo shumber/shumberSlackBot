3e4f8aaec3d88b100eb5b82a5d10b56515bf609c
Scott Humber <scott@vitalitics.ca>
2017-11-02 20:29:23 -0230

    Created class for IdleRPGBot, and called class from shumberSlackBot.py

--- IdleRPGBot.py
+++ IdleRPGBot.py
@@ -6,54 +6,49 @@ import time
 import pickle
 import copy
 
-load_dotenv('keys.env')
-
-slack_token = os.environ['SLACK_API_TOKEN']
-sc = SlackClient(slack_token)
-userList = {}
-
-class idleRPGBot():
+class IdleRPGBot():
     def __init__(self, slack_token, activeChannelName, dbFileName):
-    self.slack_token = slack_token
-    self.activeChannelName = active_channel_name
-    self.sc = SlackClient(slack_token)
-    self.users = {}
-    self.fb_filename = db_filename
-    self.load()
+        self.slack_token = slack_token
+        self.activeChannelName = activeChannelName
+        self.sc = SlackClient(slack_token)
+        self.users = {}
+        self.userList = {}
+        self.fb_filename = dbFileName
+        #self.load()
 
     def handlePresenceChange(self, event):
         if event['presence'] == 'active':
-            print("Status Active for ", event['user'], " - ", userList[event['user']]['name'])
-            userList[event['user']]['active'] = time.time()
-            userList[event['user']]['activeFlag'] = True #Flag set when presence changes to active
+            print("Status Active for ", event['user'], " - ", self.userList[event['user']]['name'])
+            self.userList[event['user']]['active'] = time.time()
+            self.userList[event['user']]['activeFlag'] = True #Flag set when presence changes to active
         if event['presence'] == 'away':
-            print("Status Away for ", event['user'], " - ", userList[event['user']]['name'])
-            userList[event['user']]['away'] = time.time()
-            userList[event['user']]['activeFlag'] = False #Flag reset when presence changes to away
-            userList[event['user']]['total'] = userList[event['user']]['total'] + (userList[event['user']]['away'] - userList[event['user']]['active'])
+            print("Status Away for ", event['user'], " - ", self.userList[event['user']]['name'])
+            self.userList[event['user']]['away'] = time.time()
+            self.userList[event['user']]['activeFlag'] = False #Flag reset when presence changes to away
+            self.userList[event['user']]['total'] = self.userList[event['user']]['total'] + (self.userList[event['user']]['away'] - self.userList[event['user']]['active'])
 
 
     def RPGScore(self, event):
-        text = "Hi, " + userList[event['user']]['name'] + "! The current RPG User scores are:"
-        for key, value in userList.items():
+        text = "Hi, " + self.userList[event['user']]['name'] + "! The current RPG User scores are:"
+        for key, value in self.userList.items():
             if value['isBot'] == False:
                 if value['activeFlag'] == True: #if a user is active, we want to include their current active time in score without having to wait for a status change to away.
-                    totalScore = time.time() - userList[event['user']]['active']
+                    totalScore = time.time() - self.userList[event['user']]['active']
                 else:
                     totalScore = value['total'] #if not active, we just used the stored total score.
                 text += ("\n" + value['name'] + " - Score: " + str(int(totalScore)))            
         channel = "#bot_playground"
-        printMessageToChannel(text, channel)
+        self.printMessageToChannel(text, channel)
 
     def botHelp(self, event):
-        text = "Hi, " + userList[event['user']]['name'] + "! I can help you with the following commands:"
+        text = "Hi, " + self.userList[event['user']]['name'] + "! I can help you with the following commands:"
         text += "\n   RPGScore"
         text += "\n   help"
         channel = "#bot_playground"
-        printMessageToChannel(text, channel)
+        self.printMessageToChannel(text, channel)
 
     def printMessageToChannel(self, text, channel):
-        sc.api_call(
+        self.sc.api_call(
             "chat.postMessage",
             channel=channel,
             #channel="#bot_playground",
@@ -61,40 +56,40 @@ class idleRPGBot():
             )
 
     def main(self):
-        if sc.rtm_connect(): #connect to slack 
-            api_call = sc.api_call("users.list", presence="true")
+        if self.sc.rtm_connect(): #connect to slack 
+            api_call = self.sc.api_call("users.list", presence="true")
             users = api_call.get('members')
             for user in users:
-                userList[user['id']] = {}
-                userList[user['id']]['active'] = 0.0 #Active timestamp
-                userList[user['id']]['away'] = 0.0  #Away timestamp
-                userList[user['id']]['total'] = 0.0 #Total time Idle
-                userList[user['id']]['name'] = user['real_name']
-                userList[user['id']]['activeFlag'] = False #indicates if user is currently active
-                userList[user['id']]['isBot'] = True #bot flag - True is bot, False is not
+                self.userList[user['id']] = {}
+                self.userList[user['id']]['active'] = 0.0 #Active timestamp
+                self.userList[user['id']]['away'] = 0.0  #Away timestamp
+                self.userList[user['id']]['total'] = 0.0 #Total time Idle
+                self.userList[user['id']]['name'] = user['profile']['real_name']
+                self.userList[user['id']]['activeFlag'] = False #indicates if user is currently active
+                self.userList[user['id']]['isBot'] = True #bot flag - True is bot, False is not
                 if user['id'] != "USLACKBOT":
                     if user['deleted'] == False: #Ignore deleted users
                         if user['is_bot'] == False: #Ignore bots
-                            userList[user['id']]['isBot'] = False #any users at this point are not bots
+                            self.userList[user['id']]['isBot'] = False #any users at this point are not bots
                             if user['presence'] == "active": #if user is active, we want to set their active time without having to wait for a presence change.
-                                userList[user['id']]['active'] = time.time()
-                                userList[user['id']]['activeFlag'] = True 
+                                self.userList[user['id']]['active'] = time.time()
+                                self.userList[user['id']]['activeFlag'] = True 
 
             #print(userList)    
             while True:
-                events = sc.rtm_read()
+                events = self.sc.rtm_read()
                 print(events)
                 for event in events:
                     if event['type'] == "presence_change":
-                        handlePresenceChange(event)
+                        self.handlePresenceChange(event)
                     elif event['type'] == "message":
                         if "<@U7SD52QJV>" in event['text']:
                             if event['text'] == "<@U7SD52QJV> RPGScore":
-                                RPGScore(event)
+                                self.RPGScore(event)
                             elif "?" in event['text']:
-                                botHelp(event)
+                                self.botHelp(event)
                             elif "help" in event['text']:
-                                botHelp(event)
+                                self.botHelp(event)
                 time.sleep(1)
         else:
             print("Connection Failed")
--- shumberSlackBot.py
+++ shumberSlackBot.py
@@ -1,86 +1,11 @@
-import os
-import time
-from slackclient import SlackClient
 from dotenv import load_dotenv
+from IdleRPGBot import IdleRPGBot
+import os
 
 load_dotenv('keys.env')
-
 slack_token = os.environ['SLACK_API_TOKEN']
-sc = SlackClient(slack_token)
-userList = {}
+dbFileName = "users.db"
+activeChannelName = "#bot_playground"
 
-def handlePresenceChange(event):
-    if event['presence'] == 'active':
-        print("Status Active for ", event['user'], " - ", userList[event['user']]['name'])
-        userList[event['user']]['active'] = time.time()
-        userList[event['user']]['activeFlag'] = 1 #Flag set when presence changes to active
-    if event['presence'] == 'away':
-        print("Status Away for ", event['user'], " - ", userList[event['user']]['name'])
-        userList[event['user']]['away'] = time.time()
-        userList[event['user']]['activeFlag'] = 0 #Flag reset when presence changes to away
-        userList[event['user']]['total'] = userList[event['user']]['total'] + (userList[event['user']]['away'] - userList[event['user']]['active'])
-
-
-def handleMessage(event):
-    text = "Hi, " + userList[event['user']]['name'] + "! The current RPG User scores are:"
-    for key, value in userList.items():
-        if value['isBot'] == 0:
-            if value['activeFlag'] == 1: #if a user is active, we want to include their current active time in score without having to wait for a status change to away.
-                totalScore = time.time() - userList[event['user']]['active']
-            else:
-                totalScore = value['total'] #if not active, we just used the stored total score.
-            text += ("\n" + value['name'] + " - Score: " + str(int(totalScore)))            
-    sc.api_call(
-        "chat.postMessage",
-        channel="#bot_playground",
-        text=text
-        )
-
-def botHelp(event):
-    text = "Hi, " + userList[event['user']]['name'] + "! I can help you with the following commands:"
-    text += "\n   RPGScore"
-    text += "\n   help"
-    sc.api_call(
-        "chat.postMessage",
-        channel="#bot_playground",
-        text=text
-        )
-
-
-if sc.rtm_connect(): #connect to slack 
-    api_call = sc.api_call("users.list", presence="true")
-    users = api_call.get('members')
-    for user in users:
-        userList[user['id']] = {}
-        userList[user['id']]['active'] = 0.0
-        userList[user['id']]['away'] = 0.0
-        userList[user['id']]['total'] = 0.0
-        userList[user['id']]['name'] = user['real_name']
-        userList[user['id']]['activeFlag'] = 0 #indicates if user is currently active
-        userList[user['id']]['isBot'] = 1 #bot flag
-        if user['id'] != "USLACKBOT":
-            if user['deleted'] == False: #Ignore deleted users
-                if user['is_bot'] == False: #Ignore bots
-                    userList[user['id']]['isBot'] = 0 #any users at this point are not bots
-                    if user['presence'] == "active": #if user is active, we want to set their active time without having to wait for a presence change.
-                        userList[user['id']]['active'] = time.time()
-                        userList[user['id']]['activeFlag'] = 1 
-
-    #print(userList)    
-    while True:
-        events = sc.rtm_read()
-        print(events)
-        for event in events:
-            if event['type'] == "presence_change":
-                handlePresenceChange(event)
-            elif event['type'] == "message":
-                if "<@U7SD52QJV>" in event['text']:
-                    if event['text'] == "<@U7SD52QJV> RPGScore":
-                        handleMessage(event)
-                    elif "?" in event['text']:
-                        botHelp(event)
-                    elif "help" in event['text']:
-                        botHelp(event)
-        time.sleep(1)
-else:
-    print("Connection Failed")
+bot = IdleRPGBot(slack_token, activeChannelName, dbFileName)
+bot.main()
